9953542199f16f903a2cd9cae7e3c4bc199d40a6
guomengtao <rinuo@qq.com>
2025-05-03 09:33:27 +0800

    add run in container shell ,default is npm run build

---
+++ run-in-container.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+CONTAINER_NAME="vue3-test_vue-app_1"
+DEFAULT_CMD="npm run build"
+
+if [ $# -eq 0 ]; then
+  CMD=$DEFAULT_CMD
+else
+  CMD="$@"
+fi
+
+echo "▶ Running in container [$CONTAINER_NAME]: $CMD"
+docker exec -it "$CONTAINER_NAME" sh -c "$CMD"
